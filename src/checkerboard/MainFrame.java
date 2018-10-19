420a882ef15935a20ad878be20e0a0b7422929e3
GiBenvenuto <gi_benvenuto@windowslive.com>
2018-10-19 16:44:13 -0300

    Interface - mudanças iniciais

--- src/checkerboard/MainFrame.java
+++ src/checkerboard/MainFrame.java
@@ -26,7 +26,7 @@ public class MainFrame extends JFrame {
 
 
 	public MainFrame() {
-		setSize(new Dimension(640, 480));
+		setSize(new Dimension(640, 520));
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 

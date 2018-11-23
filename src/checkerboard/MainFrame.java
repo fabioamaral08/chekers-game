a697dc71e2770b474cc8a7470ac2d8b35b1ca110
GiBenvenuto <gi_benvenuto@windowslive.com>
2018-11-23 17:45:59 -0200

    Popup_MenuBar

--- src/checkerboard/MainFrame.java
+++ src/checkerboard/MainFrame.java
@@ -1,4 +1,4 @@
-
+package checkerboard;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -6,33 +6,85 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-import checkerboard.CheckerBoard;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class MainFrame extends JFrame {
 
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem host;
+    private JMenuItem connect;
 
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainFrame frame = new MainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainFrame frame = new MainFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
+    public MainFrame() {
+        setSize(new Dimension(640, 640));
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	public MainFrame() {
-		setSize(new Dimension(640, 640));
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        CheckerBoard checkerBoard = new CheckerBoard(8, 8, 3);
+        getContentPane().add(checkerBoard, BorderLayout.CENTER);
+        createMenu();
+        this.setJMenuBar(menuBar);
+        host.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == host) {
+                    System.out.println("host");
+                }
+            }
+        });
 
+        connect.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == connect) {
+                    System.out.println("conectar");
+                }
+            }
+        });
+    }
 
-		CheckerBoard checkerBoard = new CheckerBoard(8, 8, 3);
-		getContentPane().add(checkerBoard, BorderLayout.CENTER);
-	}
+    public void createMenu() {
+        this.menuBar = new JMenuBar();
+        menu = new JMenu();
+        menu.setText("Jogar");
+        host = new JMenuItem();
+        host.setName("hostBtn");
+        host.setText("Ser host");
+
+        connect = new JMenuItem();
+        connect.setName("conectarBtn");
+        connect.setText("Conectar");
+
+        menu.add(host);
+        menu.add(connect);
+        this.menuBar.add(menu);
+        this.menuBar.setVisible(true);
+
+    }
+    
+    public void hostAction(){
+        
+    }
+    
+    public void connectAction(){
+        
+    }
 
 }
